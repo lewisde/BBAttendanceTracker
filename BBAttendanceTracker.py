d95289a40109996077af46965a140f0301df4186
David Lewis <lewisde@gmail.com>
2020-03-31 23:33:09 -0400

    Updated to use new layout, added code to autoexpand table view, and connected login button to pull details from lineedits.

--- BBAttendanceTracker.py
+++ BBAttendanceTracker.py
@@ -1,18 +1,44 @@
 from PyQt5 import QtWidgets
+from PyQt5 import QtNetwork
 
-from attendance import Ui_Dialog
-
+# from attendance import Ui_Dialog
+from untitled2 import Ui_MainWindow
+import json
 import sys
 import os
 
 class mywindow(QtWidgets.QMainWindow):
 
-    def __init__(self):
-        super(mywindow, self).__init__()
-        self.ui = Ui_Dialog()
-        self.ui.setupUi(self)
+	def __init__(self):
+		super(mywindow, self).__init__()
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
+		header = self.ui.tableWidget.horizontalHeader()       
+		header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+		header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+		header.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
+		header.setSectionResizeMode(3, QtWidgets.QHeaderView.Stretch)
+		header.setSectionResizeMode(4, QtWidgets.QHeaderView.Stretch)
+		header.setSectionResizeMode(5, QtWidgets.QHeaderView.Stretch)
 
-app = QtWidgets.QApplication([])
-application = mywindow()
-application.show()
-sys.exit(app.exec())
+		self.ui.btnLogin.clicked.connect(self.onBtnLogin)
+
+	def onBtnLogin(self, event):
+		loginurl = "https://blackboard.olivetcollege.edu/learn/api/public/v1/oauth2/token"
+		username = self.ui.txtUsername.text()
+		password = self.ui.txtPassword.text()
+		print(username,password)
+
+
+
+if __name__ == "__main__":
+	# secrets = {}
+	# with open("offline", "r") as f:
+	# 	keys = f.readline().strip().split(",")
+	# 	values = f.readline().strip().split(",")
+	# 	secrets = dict(zip(keys, values))
+
+	app = QtWidgets.QApplication([])
+	application = mywindow()
+	application.show()
+	sys.exit(app.exec())
